417d74578a3bbd767546038a5c200f5d7c3783a9
Luan <luanfernandesjijitsu-maker@gmail.com>
2026-04-15 08:30:19 -0300

    feat(dashboard): Adicionando nova folha de reda├º├úo e novo estilo

---
+++ COPIAR.bat
@@ -0,0 +1,14 @@
+@echo off
+echo ==========================================
+echo   [!] COPIANDO DADOS FALTANTES...
+echo ==========================================
+echo.
+copy /Y "C:\Users\Luan\Downloads\80simulados\simulados-extra2.js" "C:\Users\Luan\.gemini\antigravity\scratch\enem-na-pratica\src\js\simulados-extra2.js"
+copy /Y "C:\Users\Luan\.gemini\antigravity\scratch\enem-na-pratica\src\js\simulados-extra.js" "C:\Users\Luan\.gemini\antigravity\scratch\enem-na-pratica\public\src\js\simulados-extra.js"
+copy /Y "C:\Users\Luan\Downloads\80simulados\simulados-extra2.js" "C:\Users\Luan\.gemini\antigravity\scratch\enem-na-pratica\public\src\js\simulados-extra2.js"
+
+echo.
+echo ==========================================
+echo   [!] ARQUIVOS COPIADOS COM SUCESSO!
+echo ==========================================
+pause
